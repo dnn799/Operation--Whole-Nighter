72d638fe52de1e13ba4b108e46363f6b89627a7f
Ivan Kesler <mr.toxin@gmail.com>
2013-11-09 00:57:06 +0100

    -added a note... git not cooperating

--- src/game/common/Level.java
+++ src/game/common/Level.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 /**
  * Class used to keep track of the level, on both the client and the server.
- *
+ * NOTE: maybe branch level into ClientLevel and ServerLevel, have ClientLevel do the heavy lifting and have ServerLevel just do checksums to keep load off server
  */
 public class Level {
 	ArrayList<Player> players;
